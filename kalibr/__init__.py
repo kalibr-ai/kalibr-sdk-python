821ab130dd25e9b420c5020066a831e278225582
Nikolai <nikolai@kalibr.systems>
2026-03-28 20:06:21 -0700

    chore: update __version__ to 1.9.1 in __init__.py

--- kalibr/__init__.py
+++ kalibr/__init__.py
@@ -36,7 +36,7 @@ CLI Usage:
     kalibr version                       # Show version
 """
 
-__version__ = "1.9.0"
+__version__ = "1.9.1"
 
 # Auto-instrument LLM SDKs on import (can be disabled via env var)
 import os
