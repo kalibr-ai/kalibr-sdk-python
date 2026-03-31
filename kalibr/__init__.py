fd91c7223207cec520e35a953f332b10a4cf2645
Nikolai (Kalibr CTO Agent) <nikolai@kalibr.systems>
2026-03-30 20:01:51 -0700

    chore: bump version to 1.9.4

--- kalibr/__init__.py
+++ kalibr/__init__.py
@@ -36,7 +36,7 @@ CLI Usage:
     kalibr version                       # Show version
 """
 
-__version__ = "1.9.3"
+__version__ = "1.9.4"
 
 # Auto-instrument LLM SDKs on import (can be disabled via env var)
 import os
