44bbacc36641367f8881253aa476a9c748681971
Devon <devkelley@me.com>
2026-04-04 07:35:14 -0700

    fix: sync __version__ to 1.9.8 (#153)

--- kalibr/__init__.py
+++ kalibr/__init__.py
@@ -36,7 +36,7 @@ CLI Usage:
     kalibr version                       # Show version
 """
 
-__version__ = "1.9.4"
+__version__ = "1.9.8"
 
 # Auto-instrument LLM SDKs on import (can be disabled via env var)
 import os
